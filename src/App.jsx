096da0eeb34436d5c8df38c4a61b267eff29f711
chunlucia <chungaralucia681@gmail.com>
2024-08-06 22:37:28 -0300

    Creacion de archivos

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ThemePicker, useTheme } from "@teishi/bulma_theme";
+import ArticleForm from "./components/ArticleForm";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const { primary, secondary } = useTheme("state");
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div className={`box m-4 p-4 has-background-${secondary}`}>
+            <ArticleForm />
+        </div>
+    );
 }
 
-export default App
+export default App;
+
+
+
+
+
+
+
+
+/*import ArticleForm form "./components/ArticleForm";
+
+function App(){
+    return(
+        <div>
+        <ArticleForm/>
+        </div>
+    );
+}
+
+
+
+
+export default App;*/
