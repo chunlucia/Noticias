cb79c0cbef69f9d54ff5aaf645e4bc9c2810a477
Marisel Vicenta Vasquez <mariselvicenta@gmail.com>
2024-08-11 20:16:40 -0300

    agregue archivos

---
+++ src/components/MusicPlayer/SongCard.jsx
@@ -0,0 +1,43 @@
+import useFetch from "../../hooks/useFetch";
+import DeleteSongModal from "./DeleteSongModal";
+import { useState } from "react";
+
+function SongCard({ song, user_ID }) {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const { data, isLoading, isError, doFetch } = useFetch();
+
+    return (
+        <div className={`card has-background-dark columns my-1 mx-2`}>
+            <div className="card-content">
+                <div className="media">
+                    <div className="media-content">
+                        <p className={`title is-4 has-text-white`}>
+                            {song.title}
+                        </p>
+                    </div>
+                </div>
+                <div className="content">
+                    <audio controls>
+                        <source src={song.song_file} type="audio/mpeg" />
+                        Tu navegador no soporta el elemento de audio.
+                    </audio>
+                </div>
+            </div>
+            {song.owner == user_ID ? (
+                <div className="column" onClick={() => setIsModalOpen(true)}>
+                    <button className="button is-danger">Eliminar</button>
+                </div>
+            ) : null}
+            {isModalOpen ? (
+                <DeleteSongModal
+                    isOpen={isModalOpen}
+                    onClose={() => setIsModalOpen(false)}
+                    song_id={song.id}
+                    onDelete={{ data, isLoading, isError, doFetch }}
+                />
+            ) : null}
+        </div>
+    );
+}
+
+export default SongCard;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,57 @@
+import appLogo from "../assets/infosphere.png";
+import NavMenu from "./NavMenu";
+import { Link } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+function NavBar({ appName }) {
+    const { isAuthenticated } = useAuth("state");
+    const { logout } = useAuth("actions");
+    const navigate = useNavigate();
+
+    return (
+        <header>
+            <nav
+                className={"navbar has-background-dark"}
+                role="navigation"
+                aria-label="main navigation"
+            >
+                <div className="navbar-brand">
+                    <div className="columns is-vcentered">
+                        <Link className="navbar-item column" to="/">
+                            <img
+                                src={appLogo}
+                                alt="App Logo"
+                                className="image is-64x64"
+                            />
+                        </Link>
+                        <p className="column">{appName}</p>
+                    </div>
+                </div>
+                <NavMenu
+                    items={[
+                        { text: "Home", url: "/" },
+                        { text: "Songs", url: "/songs" },
+                        { text: "Profile", url: "/profile" },
+                    ]}
+                />
+                <button
+                    className={`button is-small is-primary`}
+                    onClick={
+                        isAuthenticated
+                            ? () => {
+                                  logout();
+                              }
+                            : () => {
+                                  navigate("/login");
+                              }
+                    }
+                >
+                    {isAuthenticated ? "Cerrar sesión" : "Iniciar Sesión"}
+                </button>
+            </nav>
+        </header>
+    );
+}
+
+export default NavBar;
